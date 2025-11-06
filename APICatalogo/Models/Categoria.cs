744691c373932c89392982b71f6406f33be96ca4
Matheus Ferreira Cardoso <matheus.cardoso@squadra.com.br>
2025-11-06 10:14:22 -0300

    Adição de banco de dados MySql

--- APICatalogo/Models/Categoria.cs
+++ APICatalogo/Models/Categoria.cs
@@ -1,9 +1,17 @@
-﻿namespace APICatalogo.Models;
+﻿using System.Collections.ObjectModel;
+
+namespace APICatalogo.Models;
 
 public class Categoria
 {
     public int CategoriaId { get; set; }
     public string? Nome { get; set; }
     public string? ImageUrl { get; set; }
+    public ICollection<Produto>? Produtos { get; set; }
+
+    public Categoria()
+    {
+        Produtos = new Collection<Produto>();
+    }
 
 }
