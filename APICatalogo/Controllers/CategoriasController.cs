f9897d6c1b263b067f817a2f3f85df9482a524d1
Matheus Ferreira Cardoso <matheus.cardoso@squadra.com.br>
2025-11-06 18:04:29 -0300

    Inserção de alguns métodos para o Controller Categoria

---
+++ APICatalogo/Controllers/CategoriasController.cs
@@ -0,0 +1,62 @@
+﻿using APICatalogo.Context;
+using APICatalogo.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace APICatalogo.Controllers;
+
+[Route("[controller]")]
+[ApiController]
+public class CategoriasController : ControllerBase
+{
+    private readonly AppDbContext _context;
+
+    public CategoriasController(AppDbContext context)
+    {
+        _context = context;
+    }
+
+    [HttpGet]
+    public ActionResult<IEnumerable<Categoria>> Get()
+    {
+        var categorias = _context.Categorias.ToList();
+
+        if (categorias is null)
+        {
+            return NotFound("404: Categorias não foram encontradas");
+        }
+
+        return categorias;
+    }
+
+    [HttpGet("{id:int}")]
+    public ActionResult<Categoria> Get(int id)
+    {
+        var categoria = _context.Categorias.FirstOrDefault(c => c.CategoriaId == id);
+
+        if (categoria is null)
+            return NotFound("404: Categoria não encontrada");
+
+        return categoria;
+    }
+
+    //[HttpGet("produtos-categorias")]
+    //public ActionResult GetAll()
+    //{
+    //    var categorias = _context.Categorias.ToList();
+    //    var produtos = _context.Produtos.ToList();
+
+    //    var resultado = new
+    //    {
+    //        Categoria = categorias,
+    //        Produtos = produtos
+    //    };
+
+    //    if (resultado is null)
+    //        return NotFound("404: Nenenhum resultado e nenhuma categoria encontrados");
+
+    //    return Ok(resultado);
+    //}
+
+
+}
--- APICatalogo/Controllers/ProdutosController.cs
+++ APICatalogo/Controllers/ProdutosController.cs
@@ -4,70 +4,86 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
-namespace APICatalogo.Controllers
+namespace APICatalogo.Controllers;
+
+[Route("[controller]")]
+[ApiController]
+public class ProdutosController : ControllerBase
 {
-    [Route("[controller]")]
-    [ApiController]
-    public class ProdutosController : ControllerBase
+    private readonly AppDbContext _context;
+
+    public ProdutosController(AppDbContext context)
     {
-        private readonly AppDbContext _context;
+        _context = context;
+    }
 
-        public ProdutosController(AppDbContext context)
+    [HttpGet]
+    public ActionResult<IEnumerable<Produto>> Get()
+    {
+        var produtos = _context.Produtos.ToList();
+
+        if (produtos is null)
         {
-            _context = context;
+            return NotFound("404: Produtos não encontrados");
         }
 
-        [HttpGet]
-        public ActionResult<IEnumerable<Produto>> Get()
+        return produtos;
+    }
+
+    [HttpGet("{id:int}", Name = "ObterProduto")]
+    public ActionResult<Produto> Get(int id)
+    {
+        var produto = _context.Produtos.FirstOrDefault(p => p.ProdutoId == id);
+
+        if (produto is null)
         {
-            var produtos = _context.Produtos.ToList();
-
-            if (produtos is null)
-            {
-                return NotFound("404: Produtos não encontrados");
-            }
-
-            return produtos;
+            return NotFound("404: Produto não encontrado");
         }
 
-        [HttpGet("{id:int}", Name = "ObterProduto")]
-        public ActionResult<Produto> Get(int id)
+        return produto;
+    }
+
+    [HttpPost]
+    public ActionResult Post(Produto produto)
+    {
+        if (produto is null)
+            return BadRequest();
+
+        _context.Produtos.Add(produto);
+        _context.SaveChanges();
+
+        return new CreatedAtRouteResult("ObterProduto",
+            new { id = produto.ProdutoId }, produto);
+    }
+
+    [HttpPut("{id:int}")]
+    public ActionResult Put(int id, Produto produto)
+    {
+        if (id != produto.ProdutoId)
         {
-            var produto = _context.Produtos.FirstOrDefault(p => p.ProdutoId == id);
-
-            if (produto is null)
-            {
-                return NotFound("404: Produto não encontrado");
-            }
-
-            return produto;
+            return BadRequest();
         }
 
-        [HttpPost]
-        public ActionResult Post(Produto produto)
+        _context.Entry(produto).State = EntityState.Modified;
+        _context.SaveChanges();
+
+        return Ok(produto);
+    }
+
+    [HttpDelete("{id:int}")]
+    public ActionResult Delete(int id)
+    {
+        var produto = _context.Produtos.FirstOrDefault(p => p.ProdutoId == id);
+
+        if (produto is null)
         {
-            if (produto is null)
-                return BadRequest();
-
-            _context.Produtos.Add(produto);
-            _context.SaveChanges();
-
-            return new CreatedAtRouteResult("ObterProduto",
-                new { id = produto.ProdutoId }, produto);
+            return NotFound("404: Produto não encontrado");
         }
 
-        [HttpPut("{id:int}")]
-        public ActionResult Put(int id, Produto produto)
-        {
-            if (id != produto.ProdutoId)
-            {
-                return BadRequest();
-            }
+        _context.Produtos.Remove(produto);
+        _context.SaveChanges();
 
-            _context.Entry(produto).State = EntityState.Modified;
-            _context.SaveChanges();
-
-            return Ok(produto);
-        }
+        return Ok(produto);
     }
 }
+
